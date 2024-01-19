e472d0c62a01899ae76eb71ff78d510a814fcbab
Lucas Valente <59627903+Callak95@users.noreply.github.com>
2024-01-19 11:54:40 +0000

    :feat: add customer data and interface to customer repository

---
+++ api/src/entities/CustomerData.ts
@@ -0,0 +1,7 @@
+export interface CustomerData {
+  id?: string;
+  name?: string;
+  email?: string;
+  xCoord?: number;
+  yCoord?: number;
+}
---
+++ api/src/entities/ICustomerRepository.ts
@@ -0,0 +1,8 @@
+import { Customer } from "./Customer";
+import { CustomerData } from "./CustomerData";
+
+export interface ICustomerRepository {
+  createCustomer(customer: CustomerData): Promise<Customer>;
+  getCustomerById(id: string): Promise<Customer | null>;
+  getAllCustomers(): Promise<Customer[]>
+}
