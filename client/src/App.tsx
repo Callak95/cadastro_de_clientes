a3b60f57cc47c82b2597f030a25b3e15a48c02aa
Lucas Valente <59627903+Callak95@users.noreply.github.com>
2024-01-20 12:35:08 +0000

    :refactor: include global styles in application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import { FC } from "react";
 import { AppRoutes } from "./routes";
 import { Provider } from "react-redux";
 import { store } from "./features";
+import { GlobalStyle } from "./styles";
 
 const App: FC = () => {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <AppRoutes />
     </Provider>
   );
