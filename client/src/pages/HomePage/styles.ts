0da501fe0cc7b91e15c490c185f18fe18db01cd8
Lucas Valente <59627903+Callak95@users.noreply.github.com>
2024-01-20 13:47:00 +0000

    :refactor: modify container in home page

--- client/src/pages/HomePage/styles.ts
+++ client/src/pages/HomePage/styles.ts
@@ -3,5 +3,24 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
 `;
